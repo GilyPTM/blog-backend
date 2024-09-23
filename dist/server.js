505d2ca1740f6bf924d705d5631e8ab682758e7b
GilyPTM <gily.alexandru2007@gmail.com>
2024-09-23 20:39:32 +0300

    serverjs modificare

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors"));
@@ -17,29 +18,36 @@ const commentRouter_1 = require("./routes/commentRouter");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use((0, express_session_1.default)({
+app.use(
+  (0, express_session_1.default)({
     secret: "secret",
     resave: true,
     saveUninitialized: true,
-}));
+  })
+);
 app.use(express_1.default.static(path_1.default.join(__dirname, "dist")));
 app.use((0, express_fileupload_1.default)());
 const port = process.env.PORT;
-app.use((0, cors_1.default)({
+app.use(
+  (0, cors_1.default)({
     origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-}));
+  })
+);
 app.use("/users", userRouter_1.userRouter);
 app.use("/posts", postRouter_1.postRouter);
 app.use("/admin", adminRouter_1.adminRouter);
 app.use("/contact", contactRouter_1.contactRouter);
 app.use("/comments", commentRouter_1.commentRouter);
-app.use("/uploads", express_1.default.static(path_1.default.join(__dirname + "/uploads")));
+app.use(
+  "/uploads",
+  express_1.default.static(path_1.default.join(__dirname + "/uploads"))
+);
 app.get("/", (req, res) => {
-    res.send("Express + TypeScript Server!!!!");
-    //res.sendFile(path.join(__dirname + "/acasa.html"));
+  res.send("Express + TypeScript Server!!!!");
+  //res.sendFile(path.join(__dirname + "/acasa.html"));
 });
 app.listen(port, () => {
-    console.log(`[server]: Server is running at https://localhost:${port}`);
+  console.log(`[server]: Server is running at https://localhost:${port}`);
 });
